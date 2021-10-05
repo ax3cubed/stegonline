8f6fa18ad126e4c2a7a915b4b445cd95a924fa38
Akinwole Adeola <ajunior488@gmail.com>
2021-10-05 14:06:37 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UploadComponent } from './upload/upload.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { ImageMenuComponent } from './imagemenu/imagemenu.component';
+import { ExtractMenuComponent } from './embed-extract-data/extract-menu/extract-menu.component';
+import { EmbedMenuComponent } from './embed-extract-data/embed-menu/embed-menu.component';
+import { EmbedImageComponent } from './embed-image/embed-image.component';
+import {AboutComponent  } from "./about/about.component";
+
+const routes: Routes = [
+  { path : '', redirectTo: '/upload', pathMatch: 'full' },
+  { path: 'upload', component: UploadComponent },
+  { path: 'checklist', component: ChecklistComponent },
+  { path :'about', component: AboutComponent},
+  { path: 'image', component: ImageMenuComponent },
+  { path: 'extract', component: ExtractMenuComponent },
+  { path: 'embed', component: EmbedMenuComponent },
+  { path: 'embedimage', component: EmbedImageComponent },
+	{ path: '**', redirectTo: '/upload' } // If 404, redirect back to upload screen
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
